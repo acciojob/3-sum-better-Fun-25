eb3545a0c9a90fa5adfbf8f78d69b2ab71293d75
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-04-18 11:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let ind,sum=0;
+  arr.sort((a,b)=>a-b;) ;
+	arr.map((index)=> if(arr[i]==target) ind=index;);
+	sum=arr[ind-1]+arr[ind]+arr[ind+1];
+	
 }
 
 module.exports = threeSum;
